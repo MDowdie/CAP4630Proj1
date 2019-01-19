6a0a003a9db91deeada74131180df2bba250dce9
Matthew D <n00721843@ospreys.unf.edu>
2019-01-19 13:09:12 -0500

    Fully featured State.py class (missing docstrings)

--- CAP4630Proj1/CAP4630Proj1.py
+++ CAP4630Proj1/CAP4630Proj1.py
@@ -1,1 +1,11 @@
+from State import State
+
+ex = State("Florida","Talahassee","FL",99989,"South",4)
+print ex
+
+ex2 = State("Apple","Talahassee","FL",99989,"South",4)
+
+ex3 = State("Xylo","Talahassee","FL",99989,"South",4)
+
+print ex.__gt__(ex3)
 
---
+++ CAP4630Proj1/State.py
@@ -0,0 +1,94 @@
+class State(object):
+    """description of class"""
+
+    StateName = ""
+    @property
+    def StateName(self):
+        return self._StateName
+    @StateName.setter
+    def StateName(self,state):
+        self._StateName = state
+
+    CapitolCity = ""
+    @property
+    def CapitolCity(self):
+        return self._CapitolCity
+    @CapitolCity.setter
+    def CapitolCity(self,cap):
+        self._CapitolCity = cap
+
+    Abbreviation = ""
+    @property
+    def Abbreviation(self):
+        return self._Abbreviation
+    @Abbreviation.setter
+    def Abbreviation(self,abbr):
+        self._Abbreviation = abbr
+
+    Population = 0
+    @property
+    def Population(self):
+        return self._Population
+    @Population.setter
+    def Population(self,popul):
+        self._Population = popul
+        self._PopWithCommas = '{:,}'.format(popul)
+
+    PopWithCommas = ""
+    @property
+    def PopWithCommas(self):
+        return self._PopWithCommas
+    @PopWithCommas.setter
+    def PopWithCommas(self,popul):
+        self._PopWithCommas = popul
+
+
+    Region = ""
+    @property
+    def Region(self):
+        return self._Region
+    @Region.setter
+    def Region(self,reg):
+        self._Region = reg
+
+    USHouseSeats = 0
+    @property
+    def USHouseSeats(self):
+        return self._USHouseSeats
+    @USHouseSeats.setter
+    def USHouseSeats(self,inval):
+        self._USHouseSeats = inval
+
+
+    def __init__(self,name,capitol,abbr,popul,region,houseseats):
+        self.StateName = name
+        self.CapitolCity = capitol
+        self.Abbreviation = abbr
+        self.Population = popul
+        self.PopWithCommas = '{:,}'.format(popul)
+        self.Region = region
+        self.USHouseSeats = houseseats
+
+
+    def __gt__(a,b):
+        """Based on State Names, does a occur before b when placed in alphabetical order?"""
+        if a.StateName < b.StateName:
+            return True
+        return False
+
+    def __str__(self):
+        """Return string representative of entire State object"""
+
+        output = "State Name:        "
+        output += self._StateName
+        output += "\nCapital City:      "
+        output += self._CapitolCity
+        output += "\nState Abbr:        "
+        output += self._Abbreviation
+        output += "\nState Population:  "
+        output += self._PopWithCommas
+        output += "\nRegion:            "
+        output += self._Region
+        output += "\nUS House Seats:    "
+        output += str(self._USHouseSeats)
+        return output
